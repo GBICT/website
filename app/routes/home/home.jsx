e2e446038433d295b4f1b62a660b8c91a328f34c
Hussin Almoustafa <71430169+hassoonsy2@users.noreply.github.com>
2024-05-25 19:41:19 +0200

    Update home.jsx

--- app/routes/home/home.jsx
+++ app/routes/home/home.jsx
@@ -1,3 +1,5 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import gamestackTexture2Large from '~/assets/gamestack-list-large.jpg';
 import gamestackTexture2Placeholder from '~/assets/gamestack-list-placeholder.jpg';
 import gamestackTexture2 from '~/assets/gamestack-list.jpg';
@@ -15,11 +17,9 @@ import { baseMeta } from '~/utils/meta';
 import { Intro } from './intro';
 import { Profile } from './profile';
 import { ProjectSummary } from './project-summary';
-import { useEffect, useRef, useState } from 'react';
 import config from '~/config.json';
 import styles from './home.module.css';
 
-// Prefetch draco decoader wasm
 export const links = () => {
   return [
     {
@@ -41,7 +41,7 @@ export const links = () => {
 
 export const meta = () => {
   return baseMeta({
-    title: 'Designer + Developer',
+    title: 'Empowering Your Business Vision',
     description: `Design portfolio of ${config.name} — a product designer working on web & mobile apps with a focus on motion, experience design, and accessibility.`,
   });
 };
@@ -49,126 +49,172 @@ export const meta = () => {
 export const Home = () => {
   const [visibleSections, setVisibleSections] = useState([]);
   const [scrollIndicatorHidden, setScrollIndicatorHidden] = useState(false);
+  const [forceRender, setForceRender] = useState(false);
   const intro = useRef();
   const projectOne = useRef();
   const projectTwo = useRef();
   const projectThree = useRef();
   const details = useRef();
+  const location = useLocation();
 
-  useEffect(() => {
-    const sections = [intro, projectOne, projectTwo, projectThree, details];
-
-    const sectionObserver = new IntersectionObserver(
-      (entries, observer) => {
-        entries.forEach(entry => {
-          if (entry.isIntersecting) {
-            const section = entry.target;
-            observer.unobserve(section);
-            if (visibleSections.includes(section)) return;
-            setVisibleSections(prevSections => [...prevSections, section]);
-          }
-        });
+  export const links = () => {
+    return [
+      {
+        rel: 'prefetch',
+        href: '/draco/draco_wasm_wrapper.js',
+        as: 'script',
+        type: 'text/javascript',
+        importance: 'low',
       },
-      { rootMargin: '0px 0px -10% 0px', threshold: 0.1 }
-    );
-
-    const indicatorObserver = new IntersectionObserver(
-      ([entry]) => {
-        setScrollIndicatorHidden(!entry.isIntersecting);
+      {
+        rel: 'prefetch',
+        href: '/draco/draco_decoder.wasm',
+        as: 'fetch',
+        type: 'application/wasm',
+        importance: 'low',
       },
-      { rootMargin: '-100% 0px 0px 0px' }
-    );
+    ];
+  };
 
-    sections.forEach(section => {
-      sectionObserver.observe(section.current);
+  export const meta = () => {
+    return baseMeta({
+      title: 'Empowering Your Business Vision',
+      description: `Design portfolio of ${config.name} — a product designer working on web & mobile apps with a focus on motion, experience design, and accessibility.`,
     });
+  };
 
-    indicatorObserver.observe(intro.current);
+  export const Home = () => {
+    const [visibleSections, setVisibleSections] = useState([]);
+    const [scrollIndicatorHidden, setScrollIndicatorHidden] = useState(false);
+    const intro = useRef();
+    const projectOne = useRef();
+    const projectTwo = useRef();
+    const projectThree = useRef();
+    const details = useRef();
+    const location = useLocation();
 
-    return () => {
-      sectionObserver.disconnect();
-      indicatorObserver.disconnect();
-    };
-  }, [visibleSections]);
+    useEffect(() => {
+      setVisibleSections([]);
+    }, [location]);
 
-  return (
-    <div className={styles.home}>
-      <Intro
-        id="intro"
-        sectionRef={intro}
-        scrollIndicatorHidden={scrollIndicatorHidden}
-      />
-      <ProjectSummary
-        id="project-1"
-        sectionRef={projectOne}
-        visible={visibleSections.includes(projectOne.current)}
-        index={1}
-        title="Designing the future of education"
-        description="Designing a platform to help educators build better online courseware"
-        buttonText="View project"
-        buttonLink="/projects/smart-sparrow"
-        model={{
-          type: 'laptop',
-          alt: 'Smart Sparrow lesson builder',
-          textures: [
-            {
-              srcSet: `${sprTexture} 1280w, ${sprTextureLarge} 2560w`,
-              placeholder: sprTexturePlaceholder,
-            },
-          ],
-        }}
-      />
-      <ProjectSummary
-        id="project-2"
-        alternate
-        sectionRef={projectTwo}
-        visible={visibleSections.includes(projectTwo.current)}
-        index={2}
-        title="Video game progress tracking"
-        description="Design and development for a video game tracking app built in React Native"
-        buttonText="View website"
-        buttonLink="https://gamestack.hamishw.com"
-        model={{
-          type: 'phone',
-          alt: 'App login screen',
-          textures: [
-            {
-              srcSet: `${gamestackTexture} 375w, ${gamestackTextureLarge} 750w`,
-              placeholder: gamestackTexturePlaceholder,
-            },
-            {
-              srcSet: `${gamestackTexture2} 375w, ${gamestackTexture2Large} 750w`,
-              placeholder: gamestackTexture2Placeholder,
-            },
-          ],
-        }}
-      />
-      <ProjectSummary
-        id="project-3"
-        sectionRef={projectThree}
-        visible={visibleSections.includes(projectThree.current)}
-        index={3}
-        title="Biomedical image collaboration"
-        description="Increasing the amount of collaboration in Slice, an app for biomedical imaging"
-        buttonText="View project"
-        buttonLink="/projects/slice"
-        model={{
-          type: 'laptop',
-          alt: 'Annotating a biomedical image in the Slice app',
-          textures: [
-            {
-              srcSet: `${sliceTexture} 800w, ${sliceTextureLarge} 1920w`,
-              placeholder: sliceTexturePlaceholder,
-            },
-          ],
-        }}
-      />
-      <Profile
-        sectionRef={details}
-        visible={visibleSections.includes(details.current)}
-        id="details"
-      />
-      <Footer />
-    </div>
-  );
-};
+    useEffect(() => {
+      const sections = [intro, projectOne, projectTwo, projectThree, details];
+
+      const sectionObserver = new IntersectionObserver(
+        (entries, observer) => {
+          entries.forEach(entry => {
+            if (entry.isIntersecting) {
+              const section = entry.target;
+              observer.unobserve(section);
+              if (visibleSections.includes(section)) return;
+              setVisibleSections(prevSections => [...prevSections, section]);
+            }
+          });
+        },
+        { rootMargin: '0px 0px -10% 0px', threshold: 0.1 }
+      );
+
+      const indicatorObserver = new IntersectionObserver(
+        ([entry]) => {
+          setScrollIndicatorHidden(!entry.isIntersecting);
+        },
+        { rootMargin: '-100% 0px 0px 0px' }
+      );
+
+      sections.forEach(section => {
+        if (section.current) {
+          sectionObserver.observe(section.current);
+        }
+      });
+
+      if (intro.current) {
+        indicatorObserver.observe(intro.current);
+      }
+
+      return () => {
+        sectionObserver.disconnect();
+        indicatorObserver.disconnect();
+      };
+    }, [visibleSections]);
+
+    return (
+      <div className={styles.home}>
+        <Intro
+          id="intro"
+          sectionRef={intro}
+          scrollIndicatorHidden={scrollIndicatorHidden}
+        />
+        <ProjectSummary
+          id="project-1"
+          sectionRef={projectOne}
+          visible={visibleSections.includes(projectOne.current)}
+          index={'Workplace Management'}
+          title="Revolutionizing Workplace Management with Cutting-Edge Digital Tools"
+          description="We redefine workplace management by integrating advanced digital tools that enhance efficiency, collaboration, and overall business operations."
+          buttonText="View more"
+          buttonLink="/projects/smart-sparrow"
+          model={{
+            type: 'laptop',
+            alt: 'Smart Sparrow lesson builder',
+            textures: [
+              {
+                srcSet: `${sprTexture} 1280w, ${sprTextureLarge} 2560w`,
+                placeholder: sprTexturePlaceholder,
+              },
+            ],
+          }}
+        />
+        <ProjectSummary
+          id="project-2"
+          alternate
+          sectionRef={projectTwo}
+          visible={visibleSections.includes(projectTwo.current)}
+          index={'Software Development'}
+          title="Pioneering Digital Solutions to Propel Your Business Forward"
+          description="We sculpt the digital future with bespoke software and innovative mobile apps designed to catapult your business into a new era."
+          buttonText="View more"
+          buttonLink="/projects/smart-sparrow"
+          model={{
+            type: 'phone',
+            alt: 'App login screen',
+            textures: [
+              {
+                srcSet: `${gamestackTexture} 375w, ${gamestackTextureLarge} 750w`,
+                placeholder: gamestackTexturePlaceholder,
+              },
+              {
+                srcSet: `${gamestackTexture2} 375w, ${gamestackTexture2Large} 750w`,
+                placeholder: gamestackTexture2Placeholder,
+              },
+            ],
+          }}
+        />
+        <ProjectSummary
+          id="project-3"
+          sectionRef={projectThree}
+          visible={visibleSections.includes(projectThree.current)}
+          index={'Data Analysis & AI'}
+          title="Advanced tools and expertise to convert data into actionable insights."
+          description="Insight-driven decision support."
+          buttonText="View more"
+          buttonLink="/projects/slice"
+          model={{
+            type: 'laptop',
+            alt: 'Annotating a biomedical image in the Slice app',
+            textures: [
+              {
+                srcSet: `${sliceTexture} 800w, ${sliceTextureLarge} 1600w`,
+                placeholder: sliceTexturePlaceholder,
+              },
+            ],
+          }}
+        />
+        <Profile
+          sectionRef={details}
+          visible={visibleSections.includes(details.current)}
+          id="details"
+        />
+        <Footer />
+      </div>
+    );
+  };
