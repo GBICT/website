bdacb82ac5ae83c4bbe10567553331909125f4b5
Hussin Almoustafa <71430169+hassoonsy2@users.noreply.github.com>
2024-05-25 20:44:57 +0200

    Update project-summary.jsx

--- app/routes/home/project-summary.jsx
+++ app/routes/home/project-summary.jsx
@@ -1,22 +1,13 @@
+import React, { Suspense, useState } from 'react';
 import { Button } from '~/components/button';
 import { Divider } from '~/components/divider';
 import { Heading } from '~/components/heading';
-import { deviceModels } from '~/components/model/device-models';
 import { Section } from '~/components/section';
 import { Text } from '~/components/text';
-import { useTheme } from '~/components/theme-provider';
-import { Transition } from '~/components/transition';
 import { Loader } from '~/components/loader';
-import { useWindowSize } from '~/hooks';
-import { Suspense, lazy, useState } from 'react';
-import { cssProps, media } from '~/utils/style';
-import { useHydrated } from '~/hooks/useHydrated';
-import katakana from './katakana.svg';
 import styles from './project-summary.module.css';
 
-const Model = lazy(() =>
-  import('~/components/model').then(module => ({ default: module.Model }))
-);
+const Model = React.lazy(() => import('~/components/model').then(module => ({ default: module.Model })));
 
 export function ProjectSummary({
                                  id,
@@ -31,59 +22,24 @@ export function ProjectSummary({
                                  alternate,
                                  ...rest
                                }) {
-  const [focused, setFocused] = useState(false);
   const [modelLoaded, setModelLoaded] = useState(false);
-  const { theme } = useTheme();
-  const { width } = useWindowSize();
-  const isHydrated = useHydrated();
   const titleId = `${id}-title`;
-  const isMobile = width <= media.tablet;
-  const svgOpacity = theme === 'light' ? 0.7 : 1;
-  const indexText = index < 10 ? `0${index}` : index;
-  const phoneSizes = `(max-width: ${media.tablet}px) 30vw, 20vw`;
-  const laptopSizes = `(max-width: ${media.tablet}px) 80vw, 40vw`;
 
   function handleModelLoad() {
     setModelLoaded(true);
-  }
-
-  function renderKatakana(device, visible) {
-    return (
-      <svg
-        type="project"
-        data-visible={visible && modelLoaded}
-        data-light={theme === 'light'}
-        style={cssProps({ opacity: svgOpacity })}
-        className={styles.svg}
-        data-device={device}
-        viewBox="0 0 751 136"
-      >
-        <use href={`${katakana}#katakana-project`} />
-      </svg>
-    );
+    console.log('Model loaded');
   }
 
   function renderDetails(visible) {
     return (
       <div className={styles.details}>
         <div aria-hidden className={styles.index}>
-          <Divider
-            notchWidth="64px"
-            notchHeight="8px"
-            collapsed={!visible}
-            collapseDelay={1000}
-          />
+          <Divider notchWidth="64px" notchHeight="8px" collapsed={!visible} collapseDelay={1000} />
           <span className={styles.indexNumber} data-visible={visible}>
-            {indexText}
+            {index}
           </span>
         </div>
-        <Heading
-          level={3}
-          as="h2"
-          className={styles.title}
-          data-visible={visible}
-          id={titleId}
-        >
+        <Heading level={3} as="h2" className={styles.title} data-visible={visible} id={titleId}>
           {title}
         </Heading>
         <Text className={styles.description} data-visible={visible} as="p">
@@ -102,73 +58,63 @@ export function ProjectSummary({
     return (
       <div className={styles.preview}>
         {model.type === 'laptop' && (
-          <>
-            {renderKatakana('laptop', visible)}
-            <div className={styles.model} data-device="laptop">
-              {!modelLoaded && (
-                <Loader center className={styles.loader} data-visible={visible} />
-              )}
-              {isHydrated && visible && (
-                <Suspense fallback={<Loader center className={styles.loader} data-visible={visible} />}>
-                  <Model
-                    alt={model.alt}
-                    cameraPosition={{ x: 0, y: 0, z: 8 }}
-                    showDelay={700}
-                    onLoad={handleModelLoad}
-                    show={visible}
-                    models={[
-                      {
-                        ...deviceModels.laptop,
-                        texture: {
-                          ...model.textures[0],
-                          sizes: laptopSizes,
-                        },
+          <div className={styles.model} data-device="laptop">
+            {!modelLoaded && <Loader center className={styles.loader} data-visible={visible} />}
+            {visible && (
+              <Suspense fallback={<Loader center className={styles.loader} data-visible={visible} />}>
+                <Model
+                  alt={model.alt}
+                  cameraPosition={{ x: 0, y: 0, z: 8 }}
+                  showDelay={700}
+                  onLoad={handleModelLoad}
+                  show={visible}
+                  models={[
+                    {
+                      ...model,
+                      texture: {
+                        ...model.textures[0],
+                        sizes: `(max-width: 768px) 80vw, 40vw`,
                       },
-                    ]}
-                  />
-                </Suspense>
-              )}
-            </div>
-          </>
+                    },
+                  ]}
+                />
+              </Suspense>
+            )}
+          </div>
         )}
         {model.type === 'phone' && (
-          <>
-            {renderKatakana('phone', visible)}
-            <div className={styles.model} data-device="phone">
-              {!modelLoaded && (
-                <Loader center className={styles.loader} data-visible={visible} />
-              )}
-              {isHydrated && visible && (
-                <Suspense fallback={<Loader center className={styles.loader} data-visible={visible} />}>
-                  <Model
-                    alt={model.alt}
-                    cameraPosition={{ x: 0, y: 0, z: 11.5 }}
-                    showDelay={300}
-                    onLoad={handleModelLoad}
-                    show={visible}
-                    models={[
-                      {
-                        ...deviceModels.phone,
-                        position: { x: -0.6, y: 1.1, z: 0 },
-                        texture: {
-                          ...model.textures[0],
-                          sizes: phoneSizes,
-                        },
+          <div className={styles.model} data-device="phone">
+            {!modelLoaded && <Loader center className={styles.loader} data-visible={visible} />}
+            {visible && (
+              <Suspense fallback={<Loader center className={styles.loader} data-visible={visible} />}>
+                <Model
+                  alt={model.alt}
+                  cameraPosition={{ x: 0, y: 0, z: 11.5 }}
+                  showDelay={300}
+                  onLoad={handleModelLoad}
+                  show={visible}
+                  models={[
+                    {
+                      ...model,
+                      position: { x: -0.6, y: 1.1, z: 0 },
+                      texture: {
+                        ...model.textures[0],
+                        sizes: `(max-width: 768px) 30vw, 20vw`,
                       },
-                      {
-                        ...deviceModels.phone,
-                        position: { x: 0.6, y: -0.5, z: 0.3 },
-                        texture: {
-                          ...model.textures[1],
-                          sizes: phoneSizes,
-                        },
+                    },
+                    {
+                      ...model,
+                      position: { x: 0.6, y: -0.5, z: 0.3 },
+                      texture: {
+                        ...model.textures[1],
+                        sizes: `(max-width: 768px) 30vw, 20vw`,
                       },
-                    ]}
-                  />
-                </Suspense>
-              )}
-            </div>
-          </>
+                    },
+                  ]}
+                />
+              </Suspense>
+            )}
+          </div>
         )}
       </div>
     );
@@ -179,8 +125,6 @@ export function ProjectSummary({
       className={styles.summary}
       data-alternate={alternate}
       data-first={index === 1}
-      onFocus={() => setFocused(true)}
-      onBlur={() => setFocused(false)}
       as="section"
       aria-labelledby={titleId}
       ref={sectionRef}
@@ -189,24 +133,8 @@ export function ProjectSummary({
       {...rest}
     >
       <div className={styles.content}>
-        <Transition in={sectionVisible || focused}>
-          {({ visible }) => (
-            <>
-              {!alternate && !isMobile && (
-                <>
-                  {renderDetails(visible)}
-                  {renderPreview(visible)}
-                </>
-              )}
-              {(alternate || isMobile) && (
-                <>
-                  {renderPreview(visible)}
-                  {renderDetails(visible)}
-                </>
-              )}
-            </>
-          )}
-        </Transition>
+        {renderDetails(sectionVisible)}
+        {renderPreview(sectionVisible)}
       </div>
     </Section>
   );
