39dbf657d6d55c050110377d9e39c47c5f72c83a
Hussin Almoustafa <71430169+hassoonsy2@users.noreply.github.com>
2024-05-25 19:29:15 +0200

    Update home.jsx

--- app/routes/home/home.jsx
+++ app/routes/home/home.jsx
@@ -57,135 +57,164 @@ export const Home = () => {
   const details = useRef();
   const location = useLocation();
 
-  useEffect(() => {
-    setVisibleSections([]);
-  }, [location]);
-
-  useEffect(() => {
-    // Force a re-render after the initial mount
-    setForceRender(true);
-  }, []);
-
-  useEffect(() => {
-    if (!forceRender) return;
-
-    const sections = [intro, projectOne, projectTwo, projectThree, details];
-
-    const sectionObserver = new IntersectionObserver(
-      (entries, observer) => {
-        entries.forEach(entry => {
-          if (entry.isIntersecting) {
-            const section = entry.target;
-            observer.unobserve(section);
-            if (visibleSections.includes(section)) return;
-            setVisibleSections(prevSections => [...prevSections, section]);
-          }
-        });
+  export const links = () => {
+    return [
+      {
+        rel: 'prefetch',
+        href: '/draco/draco_wasm_wrapper.js',
+        as: 'script',
+        type: 'text/javascript',
+        importance: 'low',
       },
-      { rootMargin: '0px 0px -10% 0px', threshold: 0.1 }
-    );
-
-    const indicatorObserver = new IntersectionObserver(
-      ([entry]) => {
-        setScrollIndicatorHidden(!entry.isIntersecting);
+      {
+        rel: 'prefetch',
+        href: '/draco/draco_decoder.wasm',
+        as: 'fetch',
+        type: 'application/wasm',
+        importance: 'low',
       },
-      { rootMargin: '-100% 0px 0px 0px' }
-    );
+    ];
+  };
 
-    sections.forEach(section => {
-      if (section.current) {
-        sectionObserver.observe(section.current);
-      }
+  export const meta = () => {
+    return baseMeta({
+      title: 'Empowering Your Business Vision',
+      description: `Design portfolio of ${config.name} — a product designer working on web & mobile apps with a focus on motion, experience design, and accessibility.`,
     });
+  };
 
-    if (intro.current) {
-      indicatorObserver.observe(intro.current);
-    }
+  export const Home = () => {
+    const [visibleSections, setVisibleSections] = useState([]);
+    const [scrollIndicatorHidden, setScrollIndicatorHidden] = useState(false);
+    const intro = useRef();
+    const projectOne = useRef();
+    const projectTwo = useRef();
+    const projectThree = useRef();
+    const details = useRef();
+    const location = useLocation();
 
-    return () => {
-      sectionObserver.disconnect();
-      indicatorObserver.disconnect();
-    };
-  }, [visibleSections, forceRender]);
+    useEffect(() => {
+      setVisibleSections([]);
+    }, [location]);
 
-  return (
-    <div className={styles.home}>
-      <Intro
-        id="intro"
-        sectionRef={intro}
-        scrollIndicatorHidden={scrollIndicatorHidden}
-      />
-      <ProjectSummary
-        id="project-1"
-        sectionRef={projectOne}
-        visible={visibleSections.includes(projectOne.current)}
-        index={'Workplace Management'}
-        title="Revolutionizing Workplace Management with Cutting-Edge Digital Tools"
-        description="We redefine workplace management by integrating advanced digital tools that enhance efficiency, collaboration, and overall business operations."
-        buttonText="View more"
-        buttonLink="/projects/smart-sparrow"
-        model={{
-          type: 'laptop',
-          alt: 'Smart Sparrow lesson builder',
-          textures: [
-            {
-              srcSet: `${sprTexture} 1280w, ${sprTextureLarge} 2560w`,
-              placeholder: sprTexturePlaceholder,
-            },
-          ],
-        }}
-      />
-      <ProjectSummary
-        id="project-2"
-        alternate
-        sectionRef={projectTwo}
-        visible={visibleSections.includes(projectTwo.current)}
-        index={'Software Development'}
-        title="Pioneering Digital Solutions to Propel Your Business Forward"
-        description="We sculpt the digital future with bespoke software and innovative mobile apps designed to catapult your business into a new era."
-        buttonText="View more"
-        buttonLink="/projects/smart-sparrow"
-        model={{
-          type: 'phone',
-          alt: 'App login screen',
-          textures: [
-            {
-              srcSet: `${gamestackTexture} 375w, ${gamestackTextureLarge} 750w`,
-              placeholder: gamestackTexturePlaceholder,
-            },
-            {
-              srcSet: `${gamestackTexture2} 375w, ${gamestackTexture2Large} 750w`,
-              placeholder: gamestackTexture2Placeholder,
-            },
-          ],
-        }}
-      />
-      <ProjectSummary
-        id="project-3"
-        sectionRef={projectThree}
-        visible={visibleSections.includes(projectThree.current)}
-        index={'Data Analysis & AI'}
-        title="Advanced tools and expertise to convert data into actionable insights."
-        description="Insight-driven decision support."
-        buttonText="View more"
-        buttonLink="/projects/slice"
-        model={{
-          type: 'laptop',
-          alt: 'Annotating a biomedical image in the Slice app',
-          textures: [
-            {
-              srcSet: `${sliceTexture} 800w, ${sliceTextureLarge} 1600w`,
-              placeholder: sliceTexturePlaceholder,
-            },
-          ],
-        }}
-      />
-      <Profile
-        sectionRef={details}
-        visible={visibleSections.includes(details.current)}
-        id="details"
-      />
-      <Footer />
-    </div>
-  );
-};
+    useEffect(() => {
+      const sections = [intro, projectOne, projectTwo, projectThree, details];
+
+      const sectionObserver = new IntersectionObserver(
+        (entries, observer) => {
+          entries.forEach(entry => {
+            if (entry.isIntersecting) {
+              const section = entry.target;
+              observer.unobserve(section);
+              if (visibleSections.includes(section)) return;
+              setVisibleSections(prevSections => [...prevSections, section]);
+            }
+          });
+        },
+        { rootMargin: '0px 0px -10% 0px', threshold: 0.1 }
+      );
+
+      const indicatorObserver = new IntersectionObserver(
+        ([entry]) => {
+          setScrollIndicatorHidden(!entry.isIntersecting);
+        },
+        { rootMargin: '-100% 0px 0px 0px' }
+      );
+
+      sections.forEach(section => {
+        if (section.current) {
+          sectionObserver.observe(section.current);
+        }
+      });
+
+      if (intro.current) {
+        indicatorObserver.observe(intro.current);
+      }
+
+      return () => {
+        sectionObserver.disconnect();
+        indicatorObserver.disconnect();
+      };
+    }, [visibleSections]);
+
+    return (
+      <div className={styles.home}>
+        <Intro
+          id="intro"
+          sectionRef={intro}
+          scrollIndicatorHidden={scrollIndicatorHidden}
+        />
+        <ProjectSummary
+          id="project-1"
+          sectionRef={projectOne}
+          visible={visibleSections.includes(projectOne.current)}
+          index={'Workplace Management'}
+          title="Revolutionizing Workplace Management with Cutting-Edge Digital Tools"
+          description="We redefine workplace management by integrating advanced digital tools that enhance efficiency, collaboration, and overall business operations."
+          buttonText="View more"
+          buttonLink="/projects/smart-sparrow"
+          model={{
+            type: 'laptop',
+            alt: 'Smart Sparrow lesson builder',
+            textures: [
+              {
+                srcSet: `${sprTexture} 1280w, ${sprTextureLarge} 2560w`,
+                placeholder: sprTexturePlaceholder,
+              },
+            ],
+          }}
+        />
+        <ProjectSummary
+          id="project-2"
+          alternate
+          sectionRef={projectTwo}
+          visible={visibleSections.includes(projectTwo.current)}
+          index={'Software Development'}
+          title="Pioneering Digital Solutions to Propel Your Business Forward"
+          description="We sculpt the digital future with bespoke software and innovative mobile apps designed to catapult your business into a new era."
+          buttonText="View more"
+          buttonLink="/projects/smart-sparrow"
+          model={{
+            type: 'phone',
+            alt: 'App login screen',
+            textures: [
+              {
+                srcSet: `${gamestackTexture} 375w, ${gamestackTextureLarge} 750w`,
+                placeholder: gamestackTexturePlaceholder,
+              },
+              {
+                srcSet: `${gamestackTexture2} 375w, ${gamestackTexture2Large} 750w`,
+                placeholder: gamestackTexture2Placeholder,
+              },
+            ],
+          }}
+        />
+        <ProjectSummary
+          id="project-3"
+          sectionRef={projectThree}
+          visible={visibleSections.includes(projectThree.current)}
+          index={'Data Analysis & AI'}
+          title="Advanced tools and expertise to convert data into actionable insights."
+          description="Insight-driven decision support."
+          buttonText="View more"
+          buttonLink="/projects/slice"
+          model={{
+            type: 'laptop',
+            alt: 'Annotating a biomedical image in the Slice app',
+            textures: [
+              {
+                srcSet: `${sliceTexture} 800w, ${sliceTextureLarge} 1600w`,
+                placeholder: sliceTexturePlaceholder,
+              },
+            ],
+          }}
+        />
+        <Profile
+          sectionRef={details}
+          visible={visibleSections.includes(details.current)}
+          id="details"
+        />
+        <Footer />
+      </div>
+    );
+  };
