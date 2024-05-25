020c5feb69af6c2c5bbde88c512918697b14efdc
Hussin Almoustafa <71430169+hassoonsy2@users.noreply.github.com>
2024-05-25 20:49:32 +0200

    changes

--- app/routes/home/home.jsx
+++ app/routes/home/home.jsx
@@ -1,33 +1,74 @@
-import gamestackTexture2Large from 'assets/Cover-2-large.png';
-import gamestackTexture2Placeholder from 'assets/gamestack-list-placeholder.jpg';
-import gamestackTexture2 from 'assets/Cover-2.png';
-import gamestackTextureLarge from 'assets/Cover_1-large.png';
-import gamestackTexturePlaceholder from 'assets/gamestack-login-placeholder.jpg';
-import gamestackTexture from 'assets/Cover_1.png';
-import sliceTextureLarge from 'assets/data-1900.png';
-import sliceTexturePlaceholder from 'assets/slice-app-placeholder.jpg';
-import sliceTexture from 'assets/data-800.png';
-import sprTextureLarge from 'assets/spr-lesson-builder-dark-large.jpg';
-import sprTexturePlaceholder from 'assets/spr-lesson-builder-dark-placeholder.jpg';
-import sprTexture from 'assets/spr-lesson-builder-dark.jpg';
-import { Footer } from 'components/Footer';
-import { Meta } from 'components/Meta';
-import { Intro } from 'layouts/Home/Intro';
-import { Profile } from 'layouts/Home/Profile';
-import { ProjectSummary } from 'layouts/Home/ProjectSummary';
-import { useEffect, useRef, useState } from 'react';
-import styles from './Home.module.css';
+import React, { useEffect, useRef, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import gamestackTexture2Large from '~/assets/gamestack-list-large.jpg';
+import gamestackTexture2Placeholder from '~/assets/gamestack-list-placeholder.jpg';
+import gamestackTexture2 from '~/assets/gamestack-list.jpg';
+import gamestackTextureLarge from '~/assets/gamestack-login-large.jpg';
+import gamestackTexturePlaceholder from '~/assets/gamestack-login-placeholder.jpg';
+import gamestackTexture from '~/assets/gamestack-login.jpg';
+import sliceTextureLarge from '~/assets/slice-app-large.jpg';
+import sliceTexturePlaceholder from '~/assets/slice-app-placeholder.jpg';
+import sliceTexture from '~/assets/slice-app.jpg';
+import sprTextureLarge from '~/assets/spr-lesson-builder-dark-large.jpg';
+import sprTexturePlaceholder from '~/assets/spr-lesson-builder-dark-placeholder.jpg';
+import sprTexture from '~/assets/spr-lesson-builder-dark.jpg';
+import { Footer } from '~/components/footer';
+import { baseMeta } from '~/utils/meta';
+import { Intro } from './intro';
+import { Profile } from './profile';
+import { ProjectSummary } from './project-summary';
+import config from '~/config.json';
+import styles from './home.module.css';
+
+export const links = () => {
+  return [
+    {
+      rel: 'prefetch',
+      href: '/draco/draco_wasm_wrapper.js',
+      as: 'script',
+      type: 'text/javascript',
+      importance: 'low',
+    },
+    {
+      rel: 'prefetch',
+      href: '/draco/draco_decoder.wasm',
+      as: 'fetch',
+      type: 'application/wasm',
+      importance: 'low',
+    },
+  ];
+};
+
+export const meta = () => {
+  return baseMeta({
+    title: 'Empowering Your Business Vision',
+    description: `Design portfolio of ${config.name} — a product designer working on web & mobile apps with a focus on motion, experience design, and accessibility.`,
+  });
+};
 
 export const Home = () => {
   const [visibleSections, setVisibleSections] = useState([]);
   const [scrollIndicatorHidden, setScrollIndicatorHidden] = useState(false);
+  const [forceRender, setForceRender] = useState(false);
   const intro = useRef();
   const projectOne = useRef();
   const projectTwo = useRef();
   const projectThree = useRef();
   const details = useRef();
+  const location = useLocation();
 
   useEffect(() => {
+    setVisibleSections([]);
+  }, [location]);
+
+  useEffect(() => {
+    // Force a re-render after the initial mount
+    setForceRender(true);
+  }, []);
+
+  useEffect(() => {
+    if (!forceRender) return;
+
     const sections = [intro, projectOne, projectTwo, projectThree, details];
 
     const sectionObserver = new IntersectionObserver(
@@ -52,23 +93,23 @@ export const Home = () => {
     );
 
     sections.forEach(section => {
-      sectionObserver.observe(section.current);
+      if (section.current) {
+        sectionObserver.observe(section.current);
+      }
     });
 
-    indicatorObserver.observe(intro.current);
+    if (intro.current) {
+      indicatorObserver.observe(intro.current);
+    }
 
     return () => {
       sectionObserver.disconnect();
       indicatorObserver.disconnect();
     };
-  }, [visibleSections]);
+  }, [visibleSections, forceRender]);
 
   return (
     <div className={styles.home}>
-      <Meta
-        title="Empowering Your Business Vision"
-        description="Design portfolio of [Your Name] — a product designer working on web & mobile apps with a focus on motion, experience design, and accessibility."
-      />
       <Intro
         id="intro"
         sectionRef={intro}
@@ -78,10 +119,10 @@ export const Home = () => {
         id="project-1"
         sectionRef={projectOne}
         visible={visibleSections.includes(projectOne.current)}
-        index={1}
+        index={'Workplace Management'}
         title="Revolutionizing Workplace Management with Cutting-Edge Digital Tools"
         description="We redefine workplace management by integrating advanced digital tools that enhance efficiency, collaboration, and overall business operations."
-        buttonText="View project"
+        buttonText="View more"
         buttonLink="/projects/smart-sparrow"
         model={{
           type: 'laptop',
@@ -90,7 +131,7 @@ export const Home = () => {
             {
               srcSet: `${sprTexture} 1280w, ${sprTextureLarge} 2560w`,
               placeholder: sprTexturePlaceholder,
-              style: { width: '100%', maxWidth: '600px', height: 'auto' },
+              style: { width: '75%', maxWidth: '600px', height: 'auto' }
             },
           ],
         }}
@@ -100,10 +141,10 @@ export const Home = () => {
         alternate
         sectionRef={projectTwo}
         visible={visibleSections.includes(projectTwo.current)}
-        index={2}
+        index={'Software Development'}
         title="Pioneering Digital Solutions to Propel Your Business Forward"
         description="We sculpt the digital future with bespoke software and innovative mobile apps designed to catapult your business into a new era."
-        buttonText="View website"
+        buttonText="View more"
         buttonLink="/projects/smart-sparrow"
         model={{
           type: 'phone',
@@ -112,12 +153,10 @@ export const Home = () => {
             {
               srcSet: `${gamestackTexture} 375w, ${gamestackTextureLarge} 750w`,
               placeholder: gamestackTexturePlaceholder,
-              style: { width: '100%', maxWidth: '300px', height: 'auto' },
             },
             {
               srcSet: `${gamestackTexture2} 375w, ${gamestackTexture2Large} 750w`,
               placeholder: gamestackTexture2Placeholder,
-              style: { width: '100%', maxWidth: '300px', height: 'auto' },
             },
           ],
         }}
@@ -126,19 +165,18 @@ export const Home = () => {
         id="project-3"
         sectionRef={projectThree}
         visible={visibleSections.includes(projectThree.current)}
-        index={3}
+        index={'Data Analysis & AI'}
         title="Advanced tools and expertise to convert data into actionable insights."
-        description="Insight-driven decision support"
-        buttonText="View project"
+        description="Insight-driven decision support."
+        buttonText="View more"
         buttonLink="/projects/slice"
         model={{
           type: 'laptop',
           alt: 'Annotating a biomedical image in the Slice app',
           textures: [
             {
-              srcSet: `${sliceTexture} 800w, ${sliceTextureLarge} 1920w`,
+              srcSet: `${sliceTexture} 800w, ${sliceTextureLarge} 1600w`,
               placeholder: sliceTexturePlaceholder,
-              style: { width: '100%', maxWidth: '600px', height: 'auto' },
             },
           ],
         }}
@@ -152,5 +190,3 @@ export const Home = () => {
     </div>
   );
 };
-
-export default Home;
