902a33b2cd17f487bfea24e85008756fda595950
nizrrt <saffierr@icloud.com>
2025-02-19 12:09:42 +0100

    tr5rt5

--- app/root.jsx
+++ app/root.jsx
@@ -124,6 +124,11 @@ export default function App() {
       `}
       </script>
        {/* End Google Tag Manager */}
+       
+       <script>function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");
+o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,
+o.onload=function(){window.trackingFunctions.onLoad({appId:"6784deb4db882903eeaeb54a"})},
+document.head.appendChild(o)}initApollo();</script>
 
         <Meta />
         <Links />
