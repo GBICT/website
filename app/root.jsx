249252599e4dd50268d1f778e8b432de48974c9e
nizrrt <saffierr@icloud.com>
2025-02-25 16:31:19 +0100

    tt

--- app/root.jsx
+++ app/root.jsx
@@ -113,36 +113,6 @@ export default function App() {
         />
         <style dangerouslySetInnerHTML={{ __html: themeStyles }} />
 
-        {/* Google Tag Manager */}
-       <script>
-       {`
-         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-TCXZ7GPS');
-      `}
-      </script>
-       {/* End Google Tag Manager */}
-       <script
-  dangerouslySetInnerHTML={{
-    __html: `
-      function initApollo(){
-        var n = Math.random().toString(36).substring(7),
-            o = document.createElement("script");
-        o.src = "https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache=" + n;
-        o.async = true;
-        o.defer = true;
-        o.onload = function(){
-          window.trackingFunctions.onLoad({appId:"6784deb4db882903eeaeb54a"})
-        };
-        document.head.appendChild(o);
-      }
-      initApollo();
-    `,
-  }}
-></script>
-
         <Meta />
         <Links />
         <link rel="canonical" href={canonicalUrl} />
